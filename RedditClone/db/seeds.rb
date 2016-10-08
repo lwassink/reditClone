7c4cccfbd015f39bed3563ad3dfad75fe7fa06f9
Luke Wassink <lwassink@gmail.com>
2016-10-07 19:14:14 -0700

    add users to seed

--- RedditClone/db/seeds.rb
+++ RedditClone/db/seeds.rb
@@ -6,17 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 15.times do
-  # Sub.create!(title: Faker::Hipster.word, description: Faker::Hipster.sentence, moderator_id: (1..7).to_a.sample)
-# end
-# 
-# AUTHOR_IDS = [1,2]
-# SUB_IDS = Sub.all.to_a.map(&:id)
-#
-# 30.times do
-#   post = Post.new(title: Faker::StarWars.character, url: Faker::StarWars.planet,
-#     content: Faker::StarWars.quote,
-#     author_id: AUTHOR_IDS.sample)
-#   post.sub_ids = SUB_IDS.sample([1,3,5,7].sample)
-#   post.save
-# end
+5.times do
+  User.create(email: "#{Faker::GameOfThrones.character}@gmail.com", password: "password")
+end
+
+AUTHOR_IDS = (1..5).to_a
+SUB_IDS = Sub.all.to_a.map(&:id)
+
+15.times do
+  Sub.create!(title: Faker::Hipster.word, description: Faker::Hipster.sentence, moderator_id: AUTHOR_IDS.sample)
+end
+
+
+30.times do
+  post = Post.new(title: Faker::StarWars.character, url: Faker::StarWars.planet,
+    content: Faker::StarWars.quote,
+    author_id: AUTHOR_IDS.sample)
+  post.sub_ids = SUB_IDS.sample([1,3,5,7].sample)
+  post.save
+end
